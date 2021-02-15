47e2f6e6a5326c2c54a61a889cfcb8ad63c273fb
Erik Johansson <erik.johansson@lmd.ipsl.fr>
2021-02-15 13:00:16 +0100

    inital commit

---
+++ compare.py
@@ -0,0 +1,13 @@
+'''
+Created on Feb 15, 2021
+
+@author: Erik Johansson
+'''
+import numpy as np
+import netCDF4 as nc  # @UnresolvedImport
+import pdb
+if __name__ == '__main__':
+    mainDir = ''
+    
+    pdb.set_trace()
+    
