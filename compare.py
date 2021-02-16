85160c4c790e8033fab378db76a6cf5160267c85
Erik.Johansson <erik.johansson@smhi.se>
2021-02-16 19:38:00 +0100

    20201-02-16
    Added getLatLon
    Started the plotting rutine
    
    Signed-off-by: Erik.Johansson <erik.johansson@smhi.se>

--- compare.py
+++ compare.py
@@ -8,6 +8,58 @@ import netCDF4 as nc  # @UnresolvedImport
 import pdb
 import os
 import h5py  # @UnresolvedImport
+
+
+try:
+    from urllib2 import urlopen
+except ImportError:
+    from urllib.request import urlopen
+from io import BytesIO
+
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+
+
+
+#----------------------------------------------------
+
+def saveLatLon(ncf, fn):
+    basename = os.path.basename(fn)
+    LatLonDir = '/scratch/erikj/Data/Nwcgeo/LatLon'
+    satname = basename.split('_')[3].lower()
+    sfn = '%s/%s_LonLat.h5' %(LatLonDir, satname)
+    if not os.path.isfile(sfn):
+        pdb.set_trace()
+        lon = ncf['lon'][:].data
+        lat = ncf['lat'][:].data
+        f = h5py.File(sfn, 'w')
+        f.create_dataset('lon', data = lon)
+        f.create_dataset('lat', data = lat)
+#             f['lon'].attrs['long_name'] = str(ncf['lon'].long_name).encode('utf-8')
+        f['lon'].attrs.__setitem__('FillValue',  ncf['lon']._FillValue)
+        f['lon'].attrs.__setitem__('long_name',  ncf['lon'].long_name)
+        f['lon'].attrs.__setitem__('units',  ncf['lon'].units)
+        f['lat'].attrs.__setitem__('FillValue',  ncf['lat']._FillValue)
+        f['lat'].attrs.__setitem__('long_name',  ncf['lat'].long_name)
+        f['lat'].attrs.__setitem__('units',  ncf['lat'].units)
+        f.close()
+    return sfn
+
+
+def getLatLon(gfn):
+#     lon = ncf['lon'][:].data
+#     lat = ncf['lat'][:].data
+    f = h5py.File(gfn, 'r')
+    lat = np.asarray(f['lat'])
+    lon = np.asarray(f['lon'])
+    lat = np.where(lat == f['lat'].attrs['FillValue'], np.nan, lat)
+    lon = np.where(lon == f['lon'].attrs['FillValue'], np.nan, lon)
+    f.close()
+    return lat, lon
+
+
+#----------------------------------------------------
+
 if __name__ == '__main__':
 #     mainDir = '/scratch/b.legras/NWCGEO-APPLI-2018.1-9/export/CMA'
     mainDir = '/scratch/b.legras/OLD-V2016/NWCGEO-APPLI-4/export/CMA'
@@ -16,37 +68,32 @@ if __name__ == '__main__':
     filename = mainDir + '/' + fil
     ncf = nc.Dataset(filename)
     
-    def createLatLon(ncf, fn):
-        basename = os.path.basename(fn)
-        saveDir = '/scratch/erikj/Data/Nwcgeo/LatLon'
-        lon = ncf['lon'][:].data
-        lat = ncf['lat'][:].data
-        satname = basename.split('_')[3].lower()
-        sfn = '%s/%s_LonLat.h5' %(saveDir, satname)
-        if not os.path.isfile(sfn):
-            f = h5py.File(sfn, 'w')
-            f.create_dataset('lon', data = lon)
-            f.create_dataset('lat', data = lat)
-            f['lon'].attrs['long_name'] = str(ncf['lon'].long_name).encode('utf-8')
-            f['lon'].attrs.__setitem__('FillValue',  ncf['lon']._FillValue)
-#             f['lon'].attrs.__setitem__('long_name',  ncf['lon'].long_name)
-            f['lon'].attrs.__setitem__('units',  ncf['lon'].units)
-            f['lat'].attrs.__setitem__('FillValue',  ncf['lat']._FillValue)
-            f['lat'].attrs.__setitem__('long_name',  ncf['lat'].long_name)
-            f['lat'].attrs.__setitem__('units',  ncf['lat'].units)
-            f.close()
-#             root_grp = nc.Dataset(sfn, 'w', format='NETCDF4')
-#             root_grp.description = 'Latitude and Longitude for %s' %satname.title()
-#             
-            
-        f5 = h5py.File(sfn, 'r')   
-        pdb.set_trace()
-    createLatLon(ncf, filename)
-    
-    
+    #: Read the required data
     cma = ncf['cma'][:].data.astype(float)
     cma[ncf['cma'][:].mask] = np.nan
     
+    #: Take care of latitude and longitude        
+    sfn = saveLatLon(ncf, filename)
+    ncf.close()
+    lat, lon = getLatLon(sfn)
+    fig1 = plt.figure()
+    ax1 = fig1.add_subplot(1,1,1, projection=ccrs.Miller(central_longitude=180.0))#projection=ccrs.PlateCarree())
+    ax1.coastlines()
+    
+    ax1.imshow(cma, extent=(np.nanmin(lon), np.nanmax(lon), np.nanmin(lat), np.nanmax(lat)))#transform=ccrs.Geostationary(satellite_height=35786000))
+    fig1.show()
+    pdb.set_trace()
+    
+    fig = plt.figure()
+    ax = plt.axes(projection=ccrs.Miller())
+    ax.coastlines()
+    ax.set_global()
+#     img, crs, extent, origin = geos_image()
+    img_proj = ccrs.Geostationary(satellite_height=35786000)
+    pdb.set_trace()
+    plt.imshow(cma, transform=img_proj, origin='lower', cmap='gray')#, extent=extent, origin=origin, cmap='gray')
+    pdb.set_trace()
+    plt.show()
     
     pdb.set_trace()
     
